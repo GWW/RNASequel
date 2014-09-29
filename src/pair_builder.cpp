2badb03e52d2632809dfd99e03106bd813c1bb35
Gavin Wilson <gavin.wilson@oicr.on.ca>
2014-09-29 11:43:59 -0400

    Fixed an incorrect samtools tag

--- src/pair_builder.cpp
+++ src/pair_builder.cpp
@@ -352,8 +352,8 @@ void PairResolver::handle_pairs_() {
                     BamRead & r2 = output[output.size() - 1];
                     p.make_pair_copy(ni + 1, kept, r1, r2);
                     double score = p.align_score();
-                    r1.tags.set_value<double>("ZS", score);
-                    r2.tags.set_value<double>("ZS", score);
+                    r1.tags.set_value<float>("ZS", score);
+                    r2.tags.set_value<float>("ZS", score);
                     /*
                     cout << output.size() << "\n";
                     cout << "    " << r1 << "\n";
